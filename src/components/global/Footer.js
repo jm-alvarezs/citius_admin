10aa83e41ebccda345e1ee49677df9390b99bfc7
Juan Manuel Alvarez <juanmanuel@disrupcion.mx>
2022-02-22 10:48:43 -0600

    fixed footer

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -2,20 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container-fluid bg-dark py-3 text-white">
-      <div className="row">
-        <div className="col-12 col-md-4 mobile-center my-2">
-          &copy; 2022 CITIUS HEALTH, FITNESS & PERFORMANCE
-        </div>
-        <div className="col-12 col-md-4 text-center my-2">
-          Términos y Condiciones
-        </div>
-        <div className="col-12 col-md-4 mobile-center text-end my-2">
-          <i className="fab fa-facebook footer-icon mx-2"></i>
-          <i className="fab fa-instagram footer-icon mx-2"></i>
-          <i className="fab fa-twitter footer-icon mx-2"></i>
-        </div>
-      </div>
+    <div className="container-fluid bg-dark py-3 text-white text-center">
+      &copy; 2022 CITIUS HEALTH, FITNESS & PERFORMANCE
     </div>
   );
 };
